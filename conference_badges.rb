6ae7df621f6c27cc771b4419c3aa474336e9a015
Andrew Bay <daddnbay@yahoo.com>
2019-04-20 23:20:51 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,12 +8,12 @@ end
 # Batch Badge Creator
 
 def batch_badge_creator(names)
-  badge_messages = []
+  badge_message = []
   names.each do |name|
     message = badge_maker(name)
-    badge_messages << message
+    badge_message << message
   end
-  badge_messages
+  badge_message
 end
 
 # Assign Room
